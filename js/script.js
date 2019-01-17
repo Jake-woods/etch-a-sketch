831a62b291841077c5c5637eeca1548ea55d0fb0
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-17 15:32:37 +0000

    Behaviour changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 const grid = document.querySelector('.grid');
 
-grid.addEventListener('mouseover', (e) => {
-   if (e.target.nodeName === 'SPAN') {
-      e.target.classList.add('activate');
+const createSqr = () => {
+   const span = document.createElement('span');
+   grid.appendChild(span);
+}
+
+const createPlayground = () => {
+   for (let i = 0; i < 1000; i++) {
+      createSqr();
    }
-});
+}
+
+createPlayground();
+
+const playgroundLoaded = () => {
+   const reset = document.querySelector('#reset');
+   const spans = document.querySelectorAll('span');
+
+   // Color the screen when mouseover
+   grid.addEventListener('mouseover', (e) => {
+      if (e.target.nodeName === 'SPAN') {
+         e.target.classList.add('activate');
+      }
+   });
+
+   // Reset screen
+   reset.addEventListener('click', () => {
+      spans.forEach((span) => {
+         if (span.classList.contains('activate')) {
+            span.classList.remove('activate');
+         }
+      })
+   })
+
+}
+
+window.onload = playgroundLoaded();
